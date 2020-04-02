531451168af06fbf55fe1a9bb4fde8790e05989c
sidkha <ryan.byloos@student.umons.ac.be>
2020-04-02 16:44:21 +0200

    Added lines drawing on file opening + clear canvas button

--- src/graphics/MainApplication.java
+++ src/graphics/MainApplication.java
@@ -10,22 +10,31 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import structure.PSTNode;
 import structure.PrioritySearchTree;
+import structure.Windower;
 
 import java.io.File;
-import java.io.IOException;
 
 public class MainApplication extends Application {
 
     public GraphicsContext gc;
-    public PrioritySearchTree tree;
 
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void draw(PrioritySearchTree tree, GraphicsContext gc) {
-        //TODO
+    public void draw(String path, GraphicsContext gc) {
+        Windower windower = new Windower(path);
+        PrioritySearchTree pst = new PrioritySearchTree(path);
+        for (PSTNode n : windower.reportInSubtree(pst)) {
+            if (n != null && n.getLinkedTo() != null)
+                gc.strokeLine(n.getX(), n.getY(), n.getLinkedTo().getX(), n.getLinkedTo().getY());
+        }
+    }
+
+    public void clearCanvas(GraphicsContext gc) {
+        gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
     }
 
     @Override
@@ -42,23 +51,26 @@ public class MainApplication extends Application {
 
         // Create MenuItems
         MenuItem openFileItem = new MenuItem("Open File");
+        MenuItem clearCanvasItem = new MenuItem("Clear canvas");
         MenuItem exitItem = new MenuItem("Exit");
         MenuItem editWindowItem = new MenuItem("Edit window");
 
         openFileItem.setOnAction(event -> {
             File file = fileChooser.showOpenDialog(stage);
             if (file != null) {
-                //TODO Extract the data from the selected file
-                tree = new PrioritySearchTree(file.getPath());
+                clearCanvas(gc);
+                System.out.println(file.getPath());
+                draw(file.getPath(), gc);
             }
         });
+        clearCanvasItem.setOnAction(event -> clearCanvas(gc));
         exitItem.setOnAction(event -> System.exit(0));
         editWindowItem.setOnAction(event -> {
             //TODO Allow the user to modify the window size
         });
 
         // Add menuItems to the Menus
-        fileMenu.getItems().addAll(openFileItem, exitItem);
+        fileMenu.getItems().addAll(openFileItem, clearCanvasItem, exitItem);
         editMenu.getItems().addAll(editWindowItem);
 
         // Add Menus to the MenuBar
